01d905ca08cbb98374fedf60c673658fe6584749
matvey <matvey.karlov@inbox.ru>
2024-08-02 21:41:02 +0300

    deploy fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import os, sys
 
-sys.path.insert(0, '/home/m/matveye7/matveye7.beget.tech/DevClassroom')
-sys.path.insert(1, '/home/m/matveye7/matveye7.beget.tech/DevClassroom')
+sys.path.insert(0, '/home/m/matveye7/matveye7.beget.tech/DevClassroom/DevClassroom')
+sys.path.insert(1, '/home/m/matveye7/matveye7.beget.tech/DevClassroom/DevClassVenv/lib/python3.11/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DevClassroom.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
